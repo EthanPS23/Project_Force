922f2361595534cf4160240009f129cd85234683
Yosuke Saito <801085@ACDM.DS.SAIT.CA>
2018-11-13 16:15:44 -0700

    Added header and footer to page

---
+++ navBar.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html>
+  <body>
+    <nav class="navbar navbar-expand-sm navbar-dark sticky-top">
+      <a class="navbar-brand" href="#">LOGO</a>
+
+      <!-- Toggler/collapsibe Button -->
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#collapsibleNavbar">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+
+      <div class="collapse navbar-collapse" id="collapsibleNavbar">
+        <ul class="navbar-nav">
+          <li class="nav-item active"><a class="nav-link" href="#">Home</a></li>
+          <li class="nav-item"><a class="nav-link" href="#">Link</a></li>
+          <li class="nav-item"><a class="nav-link" href="#">Link</a> </li>
+          <li class="nav-item dropdown">
+            <a class="nav-link dropdown-toggle" data-toggle="dropdown" id="navbardrop" href="#">More</a>
+            <div class="dropdown-menu">
+              <a class="dropdown-item" href="#">Link</a>
+              <a class="dropdown-item" href="#">Link</a>
+            </div>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  </body>
+</html>
