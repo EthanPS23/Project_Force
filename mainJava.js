670ad9b10b49f01a9ee633f602d69f9b2e2b4788
Muhammad Sagnia <793391@ACDM.DS.SAIT.CA>
2018-11-14 15:20:34 -0700

    Footer & Header Styling
    - Added more styling to footer & header (Contact_Us.html page)

---
+++ mainJava.js
@@ -0,0 +1,13 @@
+function changeColor(bgColor){
+  bgColor.style.backgroundColor = "rgba(232, 73, 73, 5)";
+}
+
+function changeColorG(bgColor){
+  bgColor.style.backgroundColor = "rgba(37, 79, 172, 0.48)";
+  bgColor.style.fontSize = "large";
+}
+
+function defaultColor(bgColor){
+  bgColor.style.backgroundColor = "transparent";
+  bgColor.style.fontSize = "medium";
+}
