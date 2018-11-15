06fd309f539e8bbad2258ddee868fb31f694c80e
Ethan Shipley <538003@ACDM.DS.SAIT.CA>
2018-11-14 21:15:15 -0700

    changes

--- stylejs.js
+++ stylejs.js
@@ -1,11 +1,11 @@
 // From site nav information used from w3schools.com/howto/howto_js_sidenav.asp
 function openNav() {
     document.getElementById("mySidenav").style.width = "150px";
-    //document.getElementById("main").style.marginLeft="250px";
+    document.getElementById("main").style.marginLeft="250px";
 
 }
 
 function closeNav() {
     document.getElementById("mySidenav").style.width = "0";
-    //document.getElementById("main").style.width = "0";
+    document.getElementById("main").style.width = "0";
 }
