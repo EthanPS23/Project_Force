315a73b5c7891f05048be0adad867a97a0ed7de4
yosk3110 <yosuke.saito@outlook.com>
2018-11-16 10:20:12 -0700

    Added index, header footer .php

---
+++ index.php
@@ -0,0 +1,18 @@
+<?php
+  $pageName = "Home";
+?>
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <link rel="stylesheet" href="mainStyle.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"></script>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <title>Main Page</title>
+  </head>
+  <body>
+
+  </body>
+</html>
