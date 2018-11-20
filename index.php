8786ccb79687e6decfe98801d96de0b8b78fc18f
Yosuke Saito <yosuke__s@hotmail.com>
2018-11-19 20:30:45 -0700

    added temp logo

--- index.php
+++ index.php
@@ -7,11 +7,11 @@
 <html lang="en" dir="ltr">
   <head>
     <meta charset="utf-8">
-    <link rel="stylesheet" href="mainStyle.css">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"></script>
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="mainStyle.css">
     <title>Main Page</title>
     <style>
     /*
@@ -30,6 +30,11 @@
     </style>
   </head>
   <body>
+    <div class="jumbotron jumbotron-fluid">
+      <div class="container">
+        <h1>Welcome to Travel Experts</h1>
+      </div>
+    </div>
     <?php include("templates/header.php"); ?>
 
     <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel" data-interval="7000">
