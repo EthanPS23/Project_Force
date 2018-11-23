92a9489912d4cee6b6f202c9050662e569e2a2f2
Ethan S <ethanps23@gmail.com>
2018-11-22 19:25:08 -0700

    formatted the login page to match that of the registration page and added comments

--- packages.php
+++ packages.php
@@ -1,3 +1,4 @@
+<!-- Packages page loads packages from the server and generates a packages card for each package being sold -->
 <?php
     $dbh = mysqli_connect("localhost","harv","password","travelexperts");
     if(!$dbh){
@@ -11,6 +12,7 @@
 <html>
 <head>
     <title><?php print($pageTitle); ?></title>
+    <!-- Packages page style sheets and javascripts -->
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"></script>
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
@@ -20,16 +22,8 @@
 </head>
 <body>
   <?php include("templates/header.php"); ?>
-    <!--<div id="mySidenav" class="sidenav">
-        <a href="javascript:void(0)" class="closebtn" onclick="closeNav()">&times;</a>
-    <div class="sidebar">
-        <a href="#packages" class="active">Packages</a>
-        <a href="indexyosuke.html">Index</a>
-        <a href="Register_Page.php">Register</a>
-        <a href="Contact_Us.html">Contact Us</a>
-    </div>-->
-    <!--<span style="font-size: 30px; cursor:pointer" class="Menu" onclick="openNav()">&#9776;Menu</span>-->
     <div class="main">
+        <!-- packages are loaded into an array and displayed individually for each package -->
         <?php
             $sql="select * from packages";
             $result =mysqli_query($dbh, $sql);
@@ -38,58 +32,41 @@
                 exit();
             }
             $firstrow=true;
+            // 
             while($row=mysqli_fetch_assoc($result)){
-                //$keys=array_keys($row);
-                $values=array_values($row);
 
+                $values=array_values($row);
+                // creates a card and when clicked would go to the package, purchase page
                 print("<div class='box' onclick=\"window.location='Register_Page.php'\">");
+                // displays ab image based on and image location received from the database
                 print("<div class='imgbx'>");
                 print("<img src='$values[7]' alt='$values[1]' class='flex-container'>");
                 print("</div>");
+                // prints out the package name
                 print("<div class='content'>");
                 print("<h2>$values[1]</h2>");
+                // prints out the package price
                 print("<div class='prices'>$" . round($values[5], 2 ) . ".00</div>");
+                // prints out the package start and end dates
                 $date1=substr($values[2],0,-9);
                 $date2=substr($values[3],0,-9);
-
+                
+                // if the start date is less than the current date then the date is turned red and crossed out
                 if((time()-(60*60*24)) < strtotime($date1)){
                     print("<div class='dates'>$date1 to $date2</div>");
                 }
                 else{
                     print("<div class='dates' style='color:red; text-decoration: line-through;'>$date1 to $date2</div>");
                 }
-
+                // prints out the package description
                 print("<p>$values[4]</p>");
                 print("</div>");
                 print("</div>");
-
-                
-
-                //print("$values[1]<br>");
-                //print("$values[2]<br>");
             }
 
         ?>
-        <!--<div class="box">
-            <div class="imgbx">
-                <div class="flex-container"></div>
-            </div>
-            <div class="content">
-                <h2>Joshua Tree</h2>
-                <div class="prices">$4800.00</div>
-                <div class="dates">2017-12-25 to 2018-01-04</div>
-
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et
-                    dolore
-                    magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
-                    commodo
-                    consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-                    pariatur..
-                </p>
-            </div>
-        </div>
-        <br>-->
     </div>
+    <?php include("templates/footer.php"); ?>
 </body>
 
 </html>
