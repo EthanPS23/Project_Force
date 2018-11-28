25c9e83afcf3e5ab896e7236e775746f28fee001
Muhammad Sagnia <793391@ACDM.DS.SAIT.CA>
2018-11-28 10:00:54 -0700

    Initialisation of Pages
    - created separate php files for add agent page and add agency page
    - included js files for validation of each pages

---
+++ addAgency.js
@@ -0,0 +1,51 @@
+//function to validate various form inputs
+
+function validate(myform)
+{
+  //agency address
+	if (myform.AgncyAddress.value == "")
+	{
+		alert("Enter an address for agency");
+		return false;
+	}
+
+  //agency city
+	if (myform.AgncyCity.value == "")
+	{
+		alert("Enter a city for agency");
+		return false;
+	}
+
+	//agency province
+	if (myform.AgncyProv.value == "")
+	{
+		alert("Please select a Province");
+		return false;
+	}
+
+	myform.AgncyPostal.value = myform.AgncyPostal.value.toUpperCase();
+	var reg3 = /^([A-Za-z]\d[A-Za-z][-]?\d[A-Za-z]\d)/;
+	if (!reg3.test(myform.AgncyPostal.value))
+	{
+		alert("Invalid postal code");
+		return false;
+	}
+
+  //agency phone number
+  var reg2 = /^[\+]?[(]?[0-9]{3}[)]?[-\s\.]?[0-9]{3}[-\s\.]?[0-9]{4,6}$/;
+	if (!reg2.test(myform.AgncyPhone.value))
+  {
+    alert("Invalid phone number format. Use: XXX-XXX-XXXX");
+    return false;
+  }
+
+	//agency fax number
+  var reg3 = /^[\+]?[(]?[0-9]{3}[)]?[-\s\.]?[0-9]{3}[-\s\.]?[0-9]{4,6}$/;
+	if (!reg2.test(myform.AgncyFax.value))
+  {
+    alert("Invalid phone number format. Use: XXX-XXX-XXXX");
+    return false;
+  }
+
+	return confirm("Continue submitting?");
+}
---
+++ addAgent.js
@@ -0,0 +1,48 @@
+function validate(myform)
+{
+  //agent first name
+	if (myform.AgtFirstName.value == "")
+	{
+		alert("Agent must have a first name");
+		return false;
+	}
+
+  //agent last name
+	if (myform.AgtLastName.value == "")
+	{
+		alert("Agent must have a last name");
+		return false;
+	}
+
+  //agent phone number
+  var reg2 = /^[\+]?[(]?[0-9]{3}[)]?[-\s\.]?[0-9]{3}[-\s\.]?[0-9]{4,6}$/;
+	if (!reg2.test(myform.AgtBusPhone.value))
+  {
+    alert("Invalid phone number format. Use: XXX-XXX-XXXX");
+    return false;
+  }
+
+  //agent email address
+  var reg = /^[a-zA-Z][a-zA-Z0-9.!#$%&'*+\/=?^_`{-]+@([a-zA-Z][a-zA-Z0-9-]+\.)+[a-zA-Z]{2,6}$/;
+	if (!reg.test(myform.AgtEmail.value))
+	{
+		alert("Invalid email address");
+		return false;
+	}
+
+  //agent position
+  if (myform.AgtPosition.value == "")
+  {
+    alert("Agent must have a position");
+    return false;
+  }
+
+  //agency of agent
+  if (myform.AgencyId.value == "")
+	{
+		alert("Select an agency for agent");
+		return false;
+	}
+
+	return confirm("Continue submitting?");
+}
