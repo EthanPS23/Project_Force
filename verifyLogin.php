85b3d2cc061b1185059bce35e97cf5843143fe14
Muhammad Sagnia <793391@ACDM.DS.SAIT.CA>
2018-11-29 09:59:57 -0700

    Login for Agents
    - agents login from login page fixed

--- verifyLogin.php
+++ verifyLogin.php
@@ -10,10 +10,22 @@
   if(mysqli_connect_errno()){
     die("Error: ".mysqli_connect_error()."!!!");
   }
+
+/*set autocommit to off*/
+  mysqli_autocommit($dbh, FALSE);
+
+  /*queries to select values*/
   $sql = "select CustomerId, CustUserId, CustPassword from customers where CustUserId=?";
-  //$sql2 = "select AgtPassword from agents where AgentId=?"; dont really need for scope of project
+  $sql2 = "select AgentId, AgtPassword from agents where AgentId=?";
+
+
   $stmt = mysqli_prepare($dbh, $sql);
-  if(!mysqli_stmt_bind_param($stmt, "s", $_REQUEST["UserId"])){
+  $stmt2 = mysqli_prepare($dbh, $sql2);
+
+  $var1 = $_REQUEST["UserId"];
+  $var2 = $_REQUEST["UserId"];
+
+  if(!mysqli_stmt_bind_param($stmt, "s", $var1)){
     $fh = fopen("errorlog.txt", "a");
     fwrite($fh, mysqli_errorno($stmt)." ");
     fwrite($fh, mysqli_stmt_error($stmt)."\n");
@@ -22,6 +34,7 @@
     $_SESSION["message"] = "Failed to connect to database";
     header("Location: login.php");
   }
+
   if(!mysqli_stmt_execute($stmt)){
     ///////should make snippet reusable
     $fh = fopen("errorlog.txt", "a");
@@ -32,11 +45,10 @@
     $_SESSION["message"] = "Failed to connect to database";
     header("Location: login.php");
   }
+  $check = false;
+
   $result = mysqli_stmt_get_result($stmt);
-
-  //password_verify($_REQUEST["CustPassword"], $password[0] == $_REQUEST["CustPassword"]);
-
-  if(($resultArray = mysqli_fetch_array($result)) && (password_verify($_REQUEST["Password"], $resultArray[2]))){
+  if(($resultArray = mysqli_fetch_array($result)) && (password_verify($_REQUEST["Password"], $resultArray[2]))) {
     $var = new Customer($resultArray);
 	$_SESSION["customer"] = $var;
 	$_SESSION["logged-in"] = true;
@@ -45,9 +57,30 @@
     unset($_SESSION["returnPage"]);
     mysqli_close($dbh);
     header("Location: $returnPage");
+    $check = true;
   }
-  else{
-    $_SESSION["message"] = "User ID or Password is incorrect";
+
+  //close first query
+  mysqli_stmt_close($stmt);
+
+  mysqli_stmt_bind_param($stmt2, "s", $var2);
+  //execute second statement
+  mysqli_stmt_execute($stmt2);
+  $result2 = mysqli_stmt_get_result($stmt2);
+
+  //password_verify($_REQUEST["CustPassword"], $password[0] == $_REQUEST["CustPassword"]);
+  //&& (password_verify($_REQUEST["Password"], $resultArray2[1]))
+  if(($resultArray2 = mysqli_fetch_array($result2)) && (password_verify($_REQUEST["Password"], $resultArray2[1])))
+  {
+    header("Location: agtDashboard.php");
+    $check = true;
+  }
+
+  mysqli_stmt_close($stmt2);
+
+
+  if($check == false){
+    $_SESSION["message"] .= " User Id or Password Incorrect";
     mysqli_close($dbh);
     header("Location: login.php");
   }
